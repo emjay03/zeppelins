9ac066434a85f074eda436bbac8c4f175a20edf6
MJ Cariso <111568194+emjay03@users.noreply.github.com>
2023-07-07 23:16:32 +0800

    shwocase

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    fontFamily: {
+      'Montserrat': ['Montserrat', 'sans-serif'],
+      
+     
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
